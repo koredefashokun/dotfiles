c459e0084d01d5250857b6f01d1ea5f540242023
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-10-02 05:00:58 +0100

    Create setup command file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# This file configures moving over dotfiles into their appropriate place on the host system.
+# (At least, that's what I'm creating it to do)
+# N.B. This file is untested, because I do not intend to automagically override my present vim config
+# (Run at your own risk)
+
+# Move the vimrc into the user's root
+mv .vimrc ~
+
